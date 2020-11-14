3c1baf3bfd8a6052d0759a80c4817fc991ef0119
alopezp90 <alopezp90@gmail.com>
2020-11-14 17:33:55 +0100

    Ej03 y Ej04 añadidos

---
+++ src/main/java/arrays/Ej03.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author Alberto López Puertas
+ * <alopezp90@gmail.com>
+ */
+public class Ej03 {
+
+    public static void main(String[] args) {
+        double[] vector = new double[20];
+        double[] vectorCopia;
+
+        Random random = new Random();
+
+        Arrays.fill(vector, 7.5);
+        vectorCopia = Arrays.copyOf(vector, 20);
+        System.out.println(Arrays.toString(vector));
+        System.out.println(Arrays.toString(vectorCopia));
+
+        if (Arrays.equals(vector, vectorCopia)) {
+            System.out.println("Los arrays son iguales");
+        } else {
+            System.out.println("Los arrays no son iguales");
+        }
+        
+        vector[random.nextInt(20)] = 6.3;
+        System.out.println(Arrays.toString(vector));
+        System.out.println(Arrays.toString(vectorCopia));
+
+        if (Arrays.equals(vector, vectorCopia)) {
+            System.out.println("Los arrays son iguales");
+        } else {
+            System.out.println("Los arrays no son iguales");
+        }
+    }
+}
---
+++ src/main/java/arrays/Ej04.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Alberto López Puertas
+ * <alopezp90@gmail.com>
+ */
+public class Ej04 {
+
+    public static void main(String[] args) {
+
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce valor (0-9)");
+        n = sc.nextInt();
+
+        for (int i = 0; i < array.length; i++) {
+            if (n == array[i]) {
+                System.out.println(n + " se encuentra en la posicion " + (i + 1) + " del array");
+                break;
+            }
+        }
+        int posicion = Arrays.binarySearch(array, n);
+        System.out.println(n + " se encuentra en la posicion " + (posicion + 1) + " del array");
+    }
+}
